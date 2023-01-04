ce543890c0147560fd1fe7d45052c5c476cd5775
Pedro Lobo <pedrocerqueiralobo@gmail.com>
2023-01-04 19:19:46 +0000

    Distance vector only fails count-to-infinity

--- src/dv.c
+++ src/dv.c
@@ -3,6 +3,7 @@
 \******************************************************************************/
 
 #include <assert.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -33,17 +34,24 @@ void *init_state() {
 
 // Notify a node that a neighboring link has changed cost.
 void notify_link_change(node_t neighbor, cost_t new_cost) {
-	assert(neighbor != get_current_node());
-
 	state_t *state = (state_t *)get_state();
 
-	// Send a message with the node's distance vector.
+	state->d[neighbor] = new_cost;
+	set_route(neighbor, neighbor, new_cost);
+
+	// Create message with distance vector.
 	message_t message;
 	message.data = (data_t *)malloc(sizeof(data_t));
 	memcpy(message.data, state->d, sizeof(data_t));
 	message.size = sizeof(message.data);
 
-	send_message(neighbor, message);
+	// Send message only to neighbors.
+	for (node_t node = get_first_node(); node <= get_last_node(); node = get_next_node(node)) {
+		cost_t link_cost = get_link_cost(node);
+		if (link_cost < COST_INFINITY && node != get_current_node()) {
+			send_message(node, message);
+		}
+	}
 }
 
 // Receive a message sent by a neighboring node.
@@ -54,23 +62,28 @@ void notify_receive_message(node_t sender, message_t message) {
 	data_t *data = (data_t *)message.data;
 
 	// Recompute distance vector.
-	int changed = 0;
+	bool changed = 0;
 	for (node_t node = get_first_node(); node <= get_last_node(); node = get_next_node(node)) {
-		if (state->d[node] > COST_ADD(get_link_cost(sender), data->d[node])) {
-			state->d[node] = COST_ADD(get_link_cost(sender), data->d[node]);
-
+		cost_t new_cost = COST_ADD(get_link_cost(sender), data->d[node]);
+		if (state->d[node] > new_cost) {
+			state->d[node] = new_cost;
 			set_route(node, sender, state->d[node]);
-
 			changed = 1;
 		}
 	}
+
+	// Create message with distance vector.
+	message_t new_message;
+	new_message.data = (data_t *)malloc(sizeof(data_t));
+	memcpy(new_message.data, state->d, sizeof(data_t));
+	new_message.size = sizeof(new_message.data);
 
 	// Send message only to neighbors.
 	if (changed) {
 		for (node_t node = get_first_node(); node <= get_last_node(); node = get_next_node(node)) {
 			cost_t link_cost = get_link_cost(node);
 			if (link_cost < COST_INFINITY && node != get_current_node()) {
-				notify_link_change(node, 0);
+				send_message(node, new_message);
 			}
 		}
 	}
